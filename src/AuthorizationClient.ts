7fa60a78ffb611b053ea56ee110b554ce8c8e1b2
Ron Seydler <ron.seydler@bentley.com>
2021-07-12 13:28:14 +1000

    updated to export issues only

--- src/AuthorizationClient.ts
+++ src/AuthorizationClient.ts
@@ -18,26 +18,9 @@ class AuthorizationClient {
       return;
     }
 
-    /*
-IMJS_AUTH_CLIENT_CLIENT_ID=spa-uqhpUbc1pk70uNDmr7lG8nHRR
-IMJS_AUTH_CLIENT_REDIRECT_URI=http://localhost:3000/signin-callback
-IMJS_AUTH_CLIENT_LOGOUT_URI=http://localhost:3000/logout
-IMJS_AUTH_CLIENT_SCOPES = "email openid profile organization assets:modify assets:read connections:modify connections:read context-registry-service:read-only forms:modify forms:read general-purpose-imodeljs-backend imodelhub imodeljs-router imodels:modify imodels:read insights:read issues:modify issues:read library:read product-settings-service projects:modify projects:read projectwise-share rbac-user:external-client realitydata:read storage:modify storage:read urlps-third-party users:read validation:modify validation:read"
-# ---- Test ids ----
-
-  
-
-    const scope = process.env.IMJS_AUTH_CLIENT_SCOPES ?? "";
-    const clientId = process.env.IMJS_AUTH_CLIENT_CLIENT_ID ?? "";
-    const redirectUri = process.env.IMJS_AUTH_CLIENT_REDIRECT_URI ?? "";
-    const postSignoutRedirectUri = process.env.IMJS_AUTH_CLIENT_LOGOUT_URI;
-*/
-
     const scope = "email openid profile organization assets:modify assets:read connections:modify connections:read context-registry-service:read-only forms:modify forms:read general-purpose-imodeljs-backend imodelhub imodeljs-router imodels:modify imodels:read insights:read issues:modify issues:read library:read product-settings-service projects:modify projects:read projectwise-share rbac-user:external-client realitydata:read storage:modify storage:read urlps-third-party users:read validation:modify validation:read";
     const clientId = "spa-xbXROps01bjtnyzjy1Z76aHWO";
-   // window.location.hostname
-  //  window.location.port
-  //  window.location.protocol
+
     const redirectUri = `${window.location.origin}/signin-callback`;
     const postSignoutRedirectUri = `${window.location.origin}/logout`;
 
--- src/Helper/IMSLoginProper.tsx
+++ src/Helper/IMSLoginProper.tsx
@@ -4,13 +4,6 @@ import { BrowserAuthorizationCallbackHandler, BrowserAuthorizationClient, Browse
 export class IMSLoginProper{
     public static async getAccessTokenForBentleyAPI(){
 
-        /*
-  const scope = "email openid profile organization assets:modify assets:read connections:modify connections:read context-registry-service:read-only forms:modify forms:read general-purpose-imodeljs-backend imodelhub imodeljs-router imodels:modify imodels:read insights:read issues:modify issues:read library:read product-settings-service projects:modify projects:read projectwise-share rbac-user:external-client realitydata:read storage:modify storage:read urlps-third-party users:read validation:modify validation:read";
-    const clientId = "spa-uqhpUbc1pk70uNDmr7lG8nHRR";
-    const redirectUri = "http://localhost:3000/signin-callback";
-    const postSignoutRedirectUri = "http://localhost:3000/logout";
-        */
-
     const oidcConfiguration: BrowserAuthorizationClientConfiguration = {
         authority: "https://ims.bentley.com",
         clientId: "spa-xbXROps01bjtnyzjy1Z76aHWO",
