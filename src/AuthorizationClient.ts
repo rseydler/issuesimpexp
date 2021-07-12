086a7f25dea8063d99a7a87e40d1a81e2e0b95fd
Ron Seydler <ron.seydler@bentley.com>
2021-07-12 13:27:46 +1000

    fixed dynamic origin

--- src/AuthorizationClient.ts
+++ src/AuthorizationClient.ts
@@ -4,6 +4,7 @@ import {
   BrowserAuthorizationClientConfiguration,
 } from "@bentley/frontend-authorization-client";
 import { FrontendRequestContext } from "@bentley/imodeljs-frontend";
+import { getWindowResizeSettings } from "@bentley/ui-ninezone";
 
 class AuthorizationClient {
   private static _oidcClient: BrowserAuthorizationClient;
@@ -34,8 +35,11 @@ IMJS_AUTH_CLIENT_SCOPES = "email openid profile organization assets:modify asset
 
     const scope = "email openid profile organization assets:modify assets:read connections:modify connections:read context-registry-service:read-only forms:modify forms:read general-purpose-imodeljs-backend imodelhub imodeljs-router imodels:modify imodels:read insights:read issues:modify issues:read library:read product-settings-service projects:modify projects:read projectwise-share rbac-user:external-client realitydata:read storage:modify storage:read urlps-third-party users:read validation:modify validation:read";
     const clientId = "spa-xbXROps01bjtnyzjy1Z76aHWO";
-    const redirectUri = "https://issuesimpexp.herokuapp.com/signin-callback";
-    const postSignoutRedirectUri = "https://issuesimpexp.herokuapp.com/logout";
+   // window.location.hostname
+  //  window.location.port
+  //  window.location.protocol
+    const redirectUri = `${window.location.origin}/signin-callback`;
+    const postSignoutRedirectUri = `${window.location.origin}/logout`;
 
     // authority is optional and will default to Production IMS
     const oidcConfiguration: BrowserAuthorizationClientConfiguration = {
